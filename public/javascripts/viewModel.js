864fdaa34325a9fe291208d6ba6092a87138c927
Ai Sugita <ai.sugita1229@gmail.com>
2014-11-15 16:56:59 +0900

    click Action complete!

--- public/javascripts/viewModel.js
+++ public/javascripts/viewModel.js
@@ -1,7 +1,4 @@
 $(function(){
-	$.getJSON("test.js", function(json){
-  		alert("JSON Data: " + json.users[3].name);
-	});
 	function myViewModel() { 
 		var self = this;
      
@@ -10,7 +7,18 @@ $(function(){
 	        { itemName: 'ワンピースあーす' , cost:'5450円', like:"false"},
 	        { itemName: 'ワンピースユニクロ', cost:'1450円',like:"false"}
 	    ]);
-	    self.likeItems = ko.observableArray([]);
+	    self.likeItems = ko.observableArray([
+	        { itemName: 'ワンピースあまぞn', },	       
+	    ]);
+	    self.clickComodityItem = function(data){
+	    	self.likeItems.push(data)
+	    	self.comodityItems.remove(data)
+	    }
+	    self.clicklikeItems = function(data){
+	    	self.comodityItems.push(data)
+	    	self.likeItems.remove(data)
+	    }
 	}
 	ko.applyBindings(myViewModel);
+
 })
